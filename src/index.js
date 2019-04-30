bb482b73523a9b45a5ff203e1f684aef16a6cd70
Tieu Luu <tieuluu@MacBook-Pro.fios-router.home>
2019-04-29 20:24:02 -0400

    initial working graphql client app

--- src/index.js
+++ src/index.js
@@ -1,12 +1,48 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import { render } from "react-dom";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import ApolloClient from "apollo-boost";
+import { ApolloProvider, Query } from "react-apollo";
+import gql from "graphql-tag";
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+const client = new ApolloClient({
+  uri: "https://us-central1-graphql-sample.cloudfunctions.net/api/graphql",
+});
+
+const Folders = () => (
+  <Query
+    query={gql`
+      {
+        folders {
+          id
+          name
+        }
+      }
+    `}
+  >
+    {({ loading, error, data }) => {
+      if (loading) return <p>Loading...</p>;
+      if (error) return <p>Error :(</p>;
+
+      return data.folders.map(({ id, name }) => (
+        <div key={id}>
+          <p>
+            {id}: {name}
+          </p>
+        </div>
+      ));
+    }}
+  </Query>
+);
+
+
+const App = () => (
+  <ApolloProvider client={client}>
+    <div>
+      <h2>My first Apollo app 🚀</h2>
+      <Folders />
+    </div>
+  </ApolloProvider>
+);
+
+render(<App />, document.getElementById("root"));
